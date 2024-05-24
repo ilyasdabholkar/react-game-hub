c2594cd10616f91a84e9a3259f161ca6fe677172
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2024-05-24 23:35:43 +0530

    Implementing Skeleton Loading For Game Cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,12 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1,2,3,4,5,6,7,8];
+
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -12,6 +15,7 @@ function GameGrid() {
         padding={10}
         spacing={10}
       >
+        {isLoading && skeletons.map(Skeleton => <GameCardSkeleton key={Skeleton}/>)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
