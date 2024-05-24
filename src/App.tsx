e4ce37c068bdb22ad0f5521dc337106e7ce5ca55
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2024-05-24 21:22:53 +0530

    Added Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+     <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
+export default App
