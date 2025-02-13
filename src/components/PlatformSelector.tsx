8af5cfff525c852af6896e12c291ab3384a14227
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-02-14 00:25:31 +0530

    Created Lookup hooks to remove duplicate lookup logic

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -8,6 +8,7 @@ import {
 } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms,{ Platform }  from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
     onSelectPlatform : (platform : Platform) => void;
@@ -16,7 +17,7 @@ interface Props {
 
 function PlatformSelector({onSelectPlatform,selectedPlatformId}:Props) {
   const { data,error } = usePlatforms();
-  const selectedPlatform = data?.results.find(p => p.id == selectedPlatformId)
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if(error) return null;
 
