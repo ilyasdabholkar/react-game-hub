5961c5a8d263317d1d08adff73011cbd982b44b5
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-02-14 00:30:39 +0530

    Simplifying StaleTime ms calculation

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import ms from 'ms';
 
 export interface Genre {
   id: number;
@@ -15,7 +16,7 @@ const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: () => apiClient.getAll(),
-    staleTime : 24 * 60 * 60 * 1000, //24 h
+    staleTime : ms('24h')
   });
 };
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import ms from 'ms';
 
 export interface Platform {
     id : number;
@@ -15,7 +16,7 @@ const usePlatforms = () => {
     return useQuery({
         queryKey : ['platforms'],
         queryFn : () => apiClient.getAll(),
-        staleTime :  24 * 60 * 60 * 1000, 
+        staleTime :  ms('24h'), 
     })
 }
 
