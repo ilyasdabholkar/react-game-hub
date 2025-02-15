24c90c68a278c5bea7bdd7d9516512abf9981dad
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-02-15 14:24:06 +0530

    Built game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,8 +18,9 @@ const GameDetailPage = () => {
     <>
       <Heading>{game?.name}</Heading>
       <ExpandableText>{String(game?.description_raw || "")}</ExpandableText>
-      <GameAttributes game={game}/>
-        <GameTrailer gameId={game?.id} />
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game?.id} />
+      <GameScreenshots gameId={game?.id} />
     </>
   );
 };
